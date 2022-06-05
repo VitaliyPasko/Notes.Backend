af8013e701c4eb55ec70883539f742ca7dd72c41
TTrueBenji <mr.lolleague@mail.ru>
2022-06-05 18:52:37 +0600

    Добавил валидацию.

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandValidation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentValidation;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandValidation : AbstractValidator<UpdateNoteCommand>
+    {
+        public UpdateNoteCommandValidation()
+        {
+            RuleFor(updateNoteCommand => updateNoteCommand.UserId).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.Id).NotEqual(Guid.Empty);
+            RuleFor(updateNoteCommand => updateNoteCommand.Title).NotEmpty().MaximumLength(250);
+        }
+    }
+}
---
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using FluentValidation;
+
+namespace Notes.Application.Notes.Queries.GetNoteList
+{
+    public class GetNoteListQueryValidator : AbstractValidator<GetNoteListQuery>
+    {
+        public GetNoteListQueryValidator()
+        {
+            RuleFor(query => query.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
